16b4543ad5737a283363467686cbdbd5b7223209
jasondellaluce <jdellalu@uci.edu>
2020-03-11 18:58:19 -0700

    initial progress

---
+++ hadoop/EdgeStatistics.java
@@ -0,0 +1,124 @@
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class EdgeStatistics {
+	
+	private static class HitStat implements Writable {
+		private long hitCounter;
+		private long totalCounter;
+		
+		protected HitStat() {
+			
+		}
+		
+		public HitStat(long hitCounter, long totalCounter) {
+			if(hitCounter < 0 || totalCounter <= 0 || hitCounter > totalCounter)
+				throw new IllegalArgumentException();
+			this.hitCounter = hitCounter;
+			this.totalCounter = totalCounter;
+		}
+
+		@Override
+		public void write(DataOutput out) throws IOException {
+			out.writeLong(hitCounter);
+			out.writeLong(totalCounter);
+		}
+		
+		@Override
+		public void readFields(DataInput in) throws IOException {
+			hitCounter = in.readLong();
+			totalCounter = in.readLong();
+		}
+
+		public static HitStat read(DataInput in) throws IOException {
+			HitStat w = new HitStat();
+			w.readFields(in);
+			return w;
+		}
+		
+		@Override
+		public String toString() {
+			return String.valueOf(getHitRatio());
+		}
+		
+		public long getHitCounter() {
+			return hitCounter;
+		}
+		
+		public long getTotalCounter() {
+			return totalCounter;
+		}
+		
+		public double getHitRatio() {
+			return ((double) hitCounter) / totalCounter;
+		}
+		
+	}
+	
+	public static class HitStatMapper extends Mapper<Object, Text, Text, HitStat> {
+		
+		private static final Pattern regexPattern = Pattern.compile(
+			"(<timestamp>){1}([0-9]*)(<\\/timestamp>){1}"
+			+ "(<fileId>){1}(.*)(<\\/fileId>){1}"
+			+ "(<cacheStatus>){1}(.*)(<\\/cacheStatus>){1}");
+		private Text fileId = new Text();
+		
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+			Matcher regexMatcher = regexPattern.matcher(value.toString());
+			if(regexMatcher.find()) {
+				String strTimestamp = regexMatcher.group(2);
+				String strFileId = regexMatcher.group(5);
+				String strCacheStatus = regexMatcher.group(8);
+				
+				fileId.set(strFileId);
+				HitStat hitStat = new HitStat(strCacheStatus.toLowerCase().contains("miss") ? 0 : 1, 1);
+				context.write(fileId, hitStat);
+			}
+		}
+	}
+
+	public static class HitStatReducer extends Reducer<Text, HitStat, Text, HitStat> {
+
+		public void reduce(Text key, Iterable<HitStat> values, Context context) throws IOException, InterruptedException {
+			long hitCounter = 0;
+			long totalCounter = 0;
+			
+			for(HitStat stat : values) {
+				hitCounter += stat.getHitCounter();
+				totalCounter += stat.getTotalCounter();
+			}
+			
+			HitStat hitStat = new HitStat(hitCounter, totalCounter);
+			context.write(key, hitStat);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "CDN Hit Statistics");
+		job.setJarByClass(EdgeStatistics.class);
+		job.setMapperClass(HitStatMapper.class);
+		job.setCombinerClass(HitStatReducer.class);
+		job.setReducerClass(HitStatReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(HitStat.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+	
+}
